fccac79848ceb177097beb12f8f2f23490774753
fQqich <125338796+fQqich@users.noreply.github.com>
2023-06-11 12:48:54 +1000

    Delete hw1 directory

--- hw1/task2.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-
-def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    with open('numbers.txt') as fi:
-        for line in fi: #we use this construction to read the first line in the file
-            data = [int(x) for x in line.split(",")]  #split the string into characters, the separator is a comma
-            Minimum = min(data) #using the built-in function to find the minimum
-            Maximum = max(data) #using the built-in function to find the maximum
-            return [Minimum, Maximum]
