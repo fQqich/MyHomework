298bfea3621af29eaba0f691a7c2dfc60cc0e63b
fQqich <125338796+fQqich@users.noreply.github.com>
2023-06-11 12:48:43 +1000

    Delete hw2 directory

--- hw2/task3.py
+++
@@ -1,6 +0,0 @@
-from typing import List, Any, Tuple
-from itertools import product
-
-def combinations(*args: List[Any]) -> List[Tuple]:
-    all_combins = product(*args)
-    return list(all_combins)
